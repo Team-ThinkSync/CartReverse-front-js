2e498a0543d41940b77628e3f43b3d7d0a65d94a
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-03-03 00:11:50 +0900

    헤더랑 푸터 로직 추가

--- src/pages/Best.jsx
+++ src/pages/Best.jsx
@@ -4,6 +4,8 @@ import { Button } from "../components/Button";
 import Clothes from "../assets/images/clothes.png";
 import { Heart, ShoppingBag } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const bestItems = [
   { 
@@ -72,6 +74,7 @@ const bestItems = [
 
     return (
       <div className="max-w-4xl mx-auto p-4 font-sans">
+        <Header />
         <div className="flex justify-between items-center mb-5">
           <h2 className="text-lg font-bold">BEST</h2>
           <span className="text-sm text-gray-500 cursor-pointer hover:underline">더보기</span>
@@ -136,6 +139,7 @@ const bestItems = [
                 </CardContent>
               </Card>
             ))}
+            <Footer/>
           </div>
         </div>
       </div>
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -3,6 +3,8 @@ import { Card, CardContent } from "../components/Card";
 import { Button } from "../components/Button"; 
 import Clothes from "../assets/images/clothes.png";
 import { TrophyIcon, ShoppingBagIcon } from "lucide-react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const categories = ["전체", "신발", "아우터", "상의", "바지"]; 
 
@@ -22,6 +24,7 @@ export default function RankingPage() {
     
   return ( 
     <div className="max-w-4xl mx-auto p-6 bg-gray-50 min-h-screen"> 
+    <Header />
       <h2 className="text-2xl font-bold mb-6 text-center">인기 상품 랭킹</h2>
       
       <div className="bg-white rounded-xl p-4 shadow-sm mb-6">
@@ -85,6 +88,7 @@ export default function RankingPage() {
           <p>해당 카테고리에 상품이 없습니다.</p>
         </div>
       )}
+      <Footer/>
     </div> 
   ); 
 }
